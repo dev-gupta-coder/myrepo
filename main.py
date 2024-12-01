4047256de5679ca44058a0cd9cf1895fc5c429f7
Dev Guptq <dev45214521@gmail.com>
2024-12-01 16:24:20 +0530

    i have some solution

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello')
+with open('dev.txt', 'r') as fobj:
+    data = fobj.read()
+
+with open('devansh.txt', 'w') as fobj:
+    newData = data.replace('java', 'python')
+    fobj.write(newData)
